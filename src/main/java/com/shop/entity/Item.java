a86244412d5c5b03e0bfd137a19d3afd8c3dd3ff
plmqazoknijb <plmqazoknijb@naver.com>
2022-10-26 03:16:46 +0900

    Completion (Chapter 6.상품 등록 및 조회하기)

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,6 +1,7 @@
 package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,26 +11,37 @@ import java.time.LocalDateTime;
 
 //Item이 데이터 베이스 테이블 역할을 하도록 만드는 어노테이션
 @Entity
-@Table(name = "item")
-@Getter
-@Setter
+@Table(name="item")
+@Getter @Setter
 @ToString
-public class Item {
-    @Id    //Id를 기본키로 사용하겠다는 것
+public class Item extends BaseEntity {
+
+    @Id
     @Column(name = "item_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id; //상품코드
+    private Long id;       //상품 코드
+
     @Column(nullable = false, length = 50)
-    private String itemNm; // 상품 이름
-    @Column(nullable = false, name="price")
+    private String itemNm; //상품명
+
+    @Column(name = "price", nullable = false)
     private int price; //가격
+
     @Column(nullable = false)
     private int stockNumber; //재고수량
+
     @Lob
     @Column(nullable = false)
     private String itemDetail; //상품 상세 설명
+
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; //상품 판매 상태
-    private LocalDateTime regTime; //등록 시간
-    private LocalDateTime updateTime; //수정 시간
+
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 }
